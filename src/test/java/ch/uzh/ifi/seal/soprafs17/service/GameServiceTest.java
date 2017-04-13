57fcfe1acc4d511bffcf20f188b75b1241da76d9
ilecipi <ilecipi@hotmail.com>
2017-04-13 12:32:22 +0200

    added test for GameResource

--- src/test/java/ch/uzh/ifi/seal/soprafs17/service/GameServiceTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs17/service/GameServiceTest.java
@@ -1,64 +1,72 @@
-//package ch.uzh.ifi.seal.soprafs17.service;
-//
-//import ch.uzh.ifi.seal.soprafs17.Application;
-//import ch.uzh.ifi.seal.soprafs17.GameConstants;
-//import ch.uzh.ifi.seal.soprafs17.constant.GameStatus;
-//import ch.uzh.ifi.seal.soprafs17.constant.UserStatus;
-//import ch.uzh.ifi.seal.soprafs17.model.entity.Game;
-//import ch.uzh.ifi.seal.soprafs17.model.entity.moves.AMove;
-//import ch.uzh.ifi.seal.soprafs17.model.entity.User;
-//import ch.uzh.ifi.seal.soprafs17.model.repository.GameRepository;
-//import ch.uzh.ifi.seal.soprafs17.model.repository.UserRepository;
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.SpringApplicationConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//
-//import static org.junit.Assert.assertNotNull;
-//
-//
-///**
-// * Created by liwitz 29.03.17
-// */
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@SpringApplicationConfiguration(classes = Application.class)
-//@Transactional
-//public class GameServiceTest {
-//
-//    @Autowired
-//    private UserRepository userRepo;
-//
-//    @Autowired
-//    private GameRepository gameRepo;
-//
-//    @Autowired
-//    private SiteBoardsService siteBoardsService;
-//
-//    @Autowired
-//    private ShipService shipService;
-//
-//    @Autowired
-//    private RoundService roundService;
-//
-//    @Autowired
-//    private GameService gameService;
-//
-//    private List<Game> games;
-//
-//    @Autowired
-//    private Game game;
-//
-//    public String addGame(game, String userToken){
-//        Assert.assertNull(gameRepo.findByName("DummyUser"));
-//        String gameStr = gameService.addGame(game, "t123");
-//        Assert.assertNotNull(gameStr);
-//    }
-//
-//
-//}
+package ch.uzh.ifi.seal.soprafs17.service;
+
+import ch.uzh.ifi.seal.soprafs17.Application;
+import ch.uzh.ifi.seal.soprafs17.GameConstants;
+import ch.uzh.ifi.seal.soprafs17.constant.GameStatus;
+import ch.uzh.ifi.seal.soprafs17.constant.UserStatus;
+import ch.uzh.ifi.seal.soprafs17.model.entity.Game;
+import ch.uzh.ifi.seal.soprafs17.model.entity.moves.AMove;
+import ch.uzh.ifi.seal.soprafs17.model.entity.User;
+import ch.uzh.ifi.seal.soprafs17.model.repository.GameRepository;
+import ch.uzh.ifi.seal.soprafs17.model.repository.UserRepository;
+import ch.uzh.ifi.seal.soprafs17.web.rest.GameResource;
+import ch.uzh.ifi.seal.soprafs17.web.rest.UserResource;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.sun.org.apache.bcel.internal.generic.LLOAD;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import static org.hamcrest.Matchers.is;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Matchers.intThat;
+import static org.mockito.Matchers.isA;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.when;
+
+
+/**
+ * Created by liwitz 29.03.17
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@Transactional
+public class GameServiceTest {
+
+
+    private List<User> testUsers;
+
+    private Game game;
+
+
+    @Test
+    public void createNewGameSuccessfully() {
+
+    }
+
+}
---
+++ src/test/java/ch/uzh/ifi/seal/soprafs17/web/rest/GameResourceTest.java
@@ -0,0 +1,150 @@
+package ch.uzh.ifi.seal.soprafs17.web.rest;
+
+import ch.uzh.ifi.seal.soprafs17.Application;
+import ch.uzh.ifi.seal.soprafs17.constant.UserStatus;
+import ch.uzh.ifi.seal.soprafs17.model.DTOs.GameDTO;
+import ch.uzh.ifi.seal.soprafs17.model.entity.Game;
+import ch.uzh.ifi.seal.soprafs17.model.entity.User;
+import ch.uzh.ifi.seal.soprafs17.model.repository.GameRepository;
+import ch.uzh.ifi.seal.soprafs17.model.repository.UserRepository;
+import ch.uzh.ifi.seal.soprafs17.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+import static sun.audio.AudioPlayer.player;
+
+/**
+ * Created by ilecipi on 13.04.17.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest({"server.port=0"})
+public class GameResourceTest {
+    @Autowired
+    private GameRepository gameRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Value("${local.server.port}")
+    private int port;
+
+    private URL base;
+    private RestTemplate template;
+
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        this.base = new URL("http://localhost:" + port + "/");
+        this.template = new TestRestTemplate();
+    }
+    @Test
+    public void addGame() throws Exception {
+        // list games with size = 0
+        List<GameDTO> gamesBefore = template.getForObject(base + "games", List.class);
+        Assert.assertTrue(gamesBefore.size() == 0);
+
+        Game gameRequest = new Game();
+        gameRequest.setName("testGame");
+        HttpEntity<Game> httpGameEntity = new HttpEntity<Game>(gameRequest);
+
+        String ownerToken = null;
+        User userRequest = new User();
+        userRequest.setName("Test");
+        userRequest.setUsername("testUser1");
+        HttpEntity<User> httpUserEntity = new HttpEntity<User>(userRequest);
+
+        // add Game with no userToken or false user Token
+        ResponseEntity<String> responseGame = template.exchange(base + "games?token=" + ownerToken, HttpMethod.POST, httpGameEntity, String.class);
+        Assert.assertEquals(null, responseGame.getBody());
+
+        //add User
+        ResponseEntity<User> responseUser = template.exchange(base + "users", HttpMethod.POST, httpUserEntity, User.class);
+        ownerToken = responseUser.getBody().getToken();
+
+        // add Game with userToken
+        responseGame = template.exchange(base + "games?token=" + ownerToken, HttpMethod.POST, httpGameEntity, String.class);
+        JSONObject jsonResponse = new JSONObject(responseGame.getBody());
+        Long gameId = Long.parseLong(jsonResponse.get("id").toString());
+        Game game = gameRepository.findOne(gameId);
+        Assert.assertEquals(game.getId().intValue(), Integer.parseInt(jsonResponse.get("id").toString()));
+
+
+    }
+
+    @Test
+    public void getGame() throws Exception {
+
+    }
+
+    @Test
+    public void startGame() throws Exception {
+
+    }
+
+    @Test
+    public void stopGame() throws Exception {
+
+    }
+
+    @Test
+    public void listPlayers() throws Exception {
+
+    }
+
+    @Test
+    public void addUser() throws Exception {
+
+    }
+
+    @Test
+    public void getPlayer() throws Exception {
+
+    }
+
+    @Test
+    public void createPlayer() throws Exception {
+
+    }
+
+}
